f9a0b8efa2b7ba97d2943ea135582e035f614a90
EmNicholson93 <Eli.m.nicholson@gmail.com>
2019-07-11 17:42:37 -0700

    refactor

--- test/items.test.js
+++ test/items.test.js
@@ -1,24 +1,6 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-// {
-//   "Name":"ford torino",
-//   "Horsepower":140,
-//   "Acceleration":10.5,
-//   "Origin":"USA"
-// },
-// {
-//   "Name":"ford galaxie 500",
-//   "Horsepower":198,
-//   "Acceleration":10,
-//   "Origin":"USA"
-// },
-// {
-//   "Name":"chevrolet impala",
-//   "Horsepower":220,
-//   "Acceleration":9,
-//   "Origin":"USA"
-// }
 // CRUD
 describe('items routes', () => {
   it('can create an item with POST', () => {
@@ -39,5 +21,49 @@ describe('items routes', () => {
           Origin: 'USA'
         });
       }); 
+      
+  });
+  it('can get an array of items with GET', () => {
+    return request(app)
+      .get('/api/v1/cars')
+      .then(res => {
+        expect(res.body).toEqual([{ 
+          Name: 'ford torino', 
+          Horsepower: 140, 
+          Acceleration: '10.5', 
+          Origin: 'USA' 
+        }]);
+      });
+  });
+  it('can GET an item by index', () => {
+    return request(app)
+      .get('/api/v1/cars/0')
+      .then(res => {
+        expect(res.body).toEqual({ 
+          Name: 'ford torino', 
+          Horsepower: 140, 
+          Acceleration: '10.5', 
+          Origin: 'USA' 
+        });
+      });
   });
 });
+    
+// {
+//   "Name":"ford torino",
+//   "Horsepower":140,
+//   "Acceleration":10.5,
+//   "Origin":"USA"
+// },
+// {
+//   "Name":"ford galaxie 500",
+//   "Horsepower":198,
+//   "Acceleration":10,
+//   "Origin":"USA"
+// },
+// {
+//   "Name":"chevrolet impala",
+//   "Horsepower":220,
+//   "Acceleration":9,
+//   "Origin":"USA"
+// }
